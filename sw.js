d43741f78fb2129a4329536b8e351642b4f62a95
dan2funk <106938900+dan2funk@users.noreply.github.com>
2026-08-28 06:34:49 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+/* Cache-first offline worker. Bump VERSION when index.html changes. */
+const VERSION = "gt-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(VERSION).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit =>
+      hit || fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(VERSION).then(c => c.put(e.request, copy));
+        return res;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
+});
